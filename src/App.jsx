3394557cb4def2625a5554368115be57c17d6f28
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2024-09-04 08:29:20 +0530

    init: vite react and react router and other dev packages

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+} from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route path="/about" element={ <About /> } />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
